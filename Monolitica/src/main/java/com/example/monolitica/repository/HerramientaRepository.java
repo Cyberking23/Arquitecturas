028fa5bec6a8c06134e956215343782214660e8f
Adrián López <ja044840@gmail.com>
2025-08-07 22:12:18 -0600

    bgfx: delete monolitica

--- Monolitica/src/main/java/com/example/monolitica/repository/HerramientaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.monolitica.repository;
-
-
-import com.example.monolitica.model.Herramienta;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HerramientaRepository extends JpaRepository<Herramienta, Long> {
-}
